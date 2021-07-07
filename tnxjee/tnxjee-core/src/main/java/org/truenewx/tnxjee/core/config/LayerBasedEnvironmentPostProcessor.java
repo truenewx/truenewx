cdf0b5bb80e16ca64153b8f4007ec13e90b576ab
jianglei <jianglei@effigo.cn>
2021-07-07 22:08:10 +0800

    分层命名的SpringBoot配置文件支持类更名

--- tnxjee/tnxjee-core/src/main/java/org/truenewx/tnxjee/core/config/LayerBasedEnvironmentPostProcessor.java
+++ tnxjee/tnxjee-core/src/main/java/org/truenewx/tnxjee/core/config/LayerBasedEnvironmentPostProcessor.java
@@ -7,7 +7,10 @@ import java.util.List;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.env.*;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.env.PropertiesPropertySourceLoader;
+import org.springframework.boot.env.PropertySourceLoader;
+import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MutablePropertySources;
 import org.springframework.core.env.PropertySource;
@@ -21,13 +24,14 @@ import org.truenewx.tnxjee.core.util.Layers;
 import org.truenewx.tnxjee.core.util.SpringUtil;
 
 /**
- * 配置属性提交处理器，用于在Spring容器启动前加载自定义的配置属性
+ * 基于分层机制的环境配置提交处理器，于Spring容器启动前加载基于分层机制的自定义配置属性
  *
  * @author jianglei
  */
-public class EnvironmentPropertyPostProcessor implements EnvironmentPostProcessor {
+public class LayerBasedEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     private static final String DEFAULT_ROOT_LOCATION = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + "config";
+    private static final String SOURCE_NAME_PREFIX = Framework.NAME + "LayerConfig: ";
 
     private ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
     private YamlPropertySourceLoader yamlLoader = new YamlPropertySourceLoader();
@@ -38,21 +42,24 @@ public class EnvironmentPropertyPostProcessor implements EnvironmentPostProcesso
         try {
             MutablePropertySources propertySources = environment.getPropertySources();
             String rootLocation = getRootLocation(environment);
+            boolean added = false;
             String profile = SpringUtil.getActiveProfile(environment);
             if (profile == null) { // 无法取得profile时，先从固定的profile.properties文件中加载profile配置，使默认profile生效
                 String profileLocation = rootLocation + Strings.SLASH + "profile.properties";
                 Resource profileResource = this.resourcePatternResolver.getResource(profileLocation);
-                addPropertySource(propertySources, rootLocation, profileResource);
+                added = addPropertySource(propertySources, rootLocation, profileResource);
             }
             // 从顶层至底层依次加载配置文件以确保上层配置优先
             for (String basename : Layers.ALL_DESC) {
-                addPropertySource(environment, rootLocation, basename);
+                added = addPropertySource(environment, rootLocation, basename) || added;
             }
 
-            System.out.println("====== Layer-based Property Sources ======");
-            for (PropertySource<?> propertySource : propertySources) {
-                if (propertySource instanceof OriginTrackedMapPropertySource) {
-                    System.out.println(propertySource);
+            if (added) {
+                System.out.println("====== Layer-based Property Sources ======");
+                for (PropertySource<?> propertySource : propertySources) {
+                    if (propertySource.getName().startsWith(SOURCE_NAME_PREFIX)) {
+                        System.out.println(propertySource.getName());
+                    }
                 }
             }
         } catch (Exception e) {
@@ -64,7 +71,7 @@ public class EnvironmentPropertyPostProcessor implements EnvironmentPostProcesso
         return environment.getProperty("spring.config.location", DEFAULT_ROOT_LOCATION);
     }
 
-    private void addPropertySource(ConfigurableEnvironment environment, String rootLocation, String basename)
+    private boolean addPropertySource(ConfigurableEnvironment environment, String rootLocation, String basename)
             throws IOException {
         List<Resource> list = new ArrayList<>();
         String location = rootLocation + Strings.SLASH + basename;
@@ -86,10 +93,12 @@ public class EnvironmentPropertyPostProcessor implements EnvironmentPostProcesso
             }
             return result;
         });
+        boolean added = false;
         MutablePropertySources propertySources = environment.getPropertySources();
         for (Resource resource : list) {
-            addPropertySource(propertySources, rootLocation, resource);
+            added = addPropertySource(propertySources, rootLocation, resource) || added;
         }
+        return added;
     }
 
     private void addResources(List<Resource> validList, String locationPattern) throws IOException {
@@ -102,22 +111,25 @@ public class EnvironmentPropertyPostProcessor implements EnvironmentPostProcesso
         }
     }
 
-    private void addPropertySource(MutablePropertySources propertySources, String rootLocation,
+    private boolean addPropertySource(MutablePropertySources propertySources, String rootLocation,
             Resource resource) throws IOException {
         if (resource.exists()) {
             String filename = resource.getFilename();
             PropertySourceLoader sourceLoader = getSourceLoader(filename);
             if (sourceLoader != null) {
-                String sourceName = Framework.NAME + "LayerConfig: [" + rootLocation + Strings.SLASH + filename + "]";
+                String sourceName = SOURCE_NAME_PREFIX + Strings.LEFT_SQUARE_BRACKET + rootLocation + Strings.SLASH
+                        + filename + Strings.RIGHT_SQUARE_BRACKET;
                 if (!propertySources.contains(sourceName)) {
                     PropertySource<?> propertySource = CollectionUtil
                             .getFirst(sourceLoader.load(sourceName, resource), null);
                     if (propertySource != null) {
                         propertySources.addLast(propertySource);
+                        return true;
                     }
                 }
             }
         }
+        return false;
     }
 
     private PropertySourceLoader getSourceLoader(String filename) {
