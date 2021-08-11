bf0a047d8ee855bdc6d3f565170e0e147a1f64f0
jianglei81 <jianglei81@sina.cn>
2021-08-11 21:56:14 +0800

    改进element样式加载顺序

--- tnxsample/tnxsample-admin/tnxsample-admin-web/src/main/webcli/src/main.js
+++ tnxsample/tnxsample-admin/tnxsample-admin-web/src/main/webcli/src/main.js
@@ -1,10 +1,10 @@
 import '../../../../../../../tnxweb/components/tnxcore.css';
 import '../../../../../../../tnxweb/components/bootstrap/base-4.5.3.css';
 import 'element-ui/lib/theme-chalk/index.css';
+import '../../../../../../../tnxweb/components/vue/element/tnxel-theme.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '../../../../../../../tnxweb/components/vue/tnxvue.css';
 import '../../../../../../../tnxweb/components/vue/element/tnxel.css';
-import '../../../../../../../tnxweb/components/vue/element/tnxel-theme.css';
 import './assets/app.css';
 import {app, Vue} from './app';
 import router from './router.js';
--- tnxsample/tnxsample-cas-web/src/main/webcli/src/libs.js
+++ tnxsample/tnxsample-cas-web/src/main/webcli/src/libs.js
@@ -1,10 +1,10 @@
 import '../../../../../../tnxweb/components/tnxcore.css';
 import '../../../../../../tnxweb/components/bootstrap/base-4.5.3.css';
 import 'element-ui/lib/theme-chalk/index.css';
+import '../../../../../../tnxweb/components/vue/element/tnxel-theme.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import '../../../../../../tnxweb/components/vue/tnxvue.css';
 import '../../../../../../tnxweb/components/vue/element/tnxel.css';
-import '../../../../../../tnxweb/components/vue/element/tnxel-theme.css';
 import tnxjq from '../../../../../../tnxweb/components/jquery/tnxjq';
 import tnxel from '../../../../../../tnxweb/components/vue/element/tnxel';
 
