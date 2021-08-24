c682147363841ee7d36559e874ab3077835c183c
jianglei81 <jianglei81@sina.cn>
2021-08-24 11:44:51 +0800

    获取用户信息的方法提升至顶级支持类

--- tnxjeex/tnxjeex-openapi/tnxjeex-openapi-client/tnxjeex-openapi-client-service/src/main/java/org/truenewx/tnxjeex/openapi/client/service/wechat/WechatAppAccessSupport.java
+++ tnxjeex/tnxjeex-openapi/tnxjeex-openapi-client/tnxjeex-openapi-client-service/src/main/java/org/truenewx/tnxjeex/openapi/client/service/wechat/WechatAppAccessSupport.java
@@ -19,6 +19,7 @@ import org.truenewx.tnxjee.core.util.HttpClientUtil;
 import org.truenewx.tnxjee.core.util.JsonUtil;
 import org.truenewx.tnxjee.core.util.tuple.Binate;
 import org.truenewx.tnxjeex.openapi.client.model.wechat.WechatAppType;
+import org.truenewx.tnxjeex.openapi.client.model.wechat.WechatUserDetail;
 
 /**
  * 微信应用访问支持
@@ -85,4 +86,32 @@ public abstract class WechatAppAccessSupport implements WechatAppAccessor {
      */
     protected abstract String getSecret();
 
+    public WechatUserDetail getUserDetail(String openId, String accessToken) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("openId", openId);
+        params.put("access_token", accessToken);
+        Map<String, Object> result = get("/sns/userinfo", params);
+        if (openId.equals(result.get("openid"))) {
+            WechatUserDetail user = new WechatUserDetail();
+            user.setAppType(getAppType());
+            user.setOpenId(openId);
+            user.setAccessToken(accessToken);
+            user.setUnionId((String) result.get("unionid"));
+            user.setHeadImageUrl((String) result.get("headimgurl"));
+            user.setNickname((String) result.get("nickname"));
+            Integer sex = (Integer) result.get("sex");
+            if (sex != null) {
+                if (sex == 1) {
+                    user.setMale(true);
+                } else if (sex == 2) {
+                    user.setMale(false);
+                }
+            }
+            user.setCountry((String) result.get("country"));
+            user.setProvince((String) result.get("province"));
+            user.setCity((String) result.get("city"));
+            return user;
+        }
+        return null;
+    }
 }
--- tnxjeex/tnxjeex-openapi/tnxjeex-openapi-client/tnxjeex-openapi-client-service/src/main/java/org/truenewx/tnxjeex/openapi/client/service/wechat/WechatOpenAppAccessSupport.java
+++ tnxjeex/tnxjeex-openapi/tnxjeex-openapi-client/tnxjeex-openapi-client-service/src/main/java/org/truenewx/tnxjeex/openapi/client/service/wechat/WechatOpenAppAccessSupport.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.truenewx.tnxjeex.openapi.client.model.wechat.WechatUser;
-import org.truenewx.tnxjeex.openapi.client.model.wechat.WechatUserDetail;
 
 /**
  * 微信开放平台（open.weixin.qq.com）应用访问支持
@@ -29,35 +28,6 @@ public abstract class WechatOpenAppAccessSupport extends WechatAppAccessSupport 
             user.setOpenId(openId);
             user.setUnionId((String) result.get("unionid"));
             user.setAccessToken((String) result.get("access_token"));
-            return user;
-        }
-        return null;
-    }
-
-    public WechatUserDetail getUserDetail(String openId, String accessToken) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("openId", openId);
-        params.put("access_token", accessToken);
-        Map<String, Object> result = get("/sns/userinfo", params);
-        if (openId.equals(result.get("openid"))) {
-            WechatUserDetail user = new WechatUserDetail();
-            user.setAppType(getAppType());
-            user.setOpenId(openId);
-            user.setAccessToken(accessToken);
-            user.setUnionId((String) result.get("unionid"));
-            user.setHeadImageUrl((String) result.get("headimgurl"));
-            user.setNickname((String) result.get("nickname"));
-            Integer sex = (Integer) result.get("sex");
-            if (sex != null) {
-                if (sex == 1) {
-                    user.setMale(true);
-                } else if (sex == 2) {
-                    user.setMale(false);
-                }
-            }
-            user.setCountry((String) result.get("country"));
-            user.setProvince((String) result.get("province"));
-            user.setCity((String) result.get("city"));
             return user;
         }
         return null;
