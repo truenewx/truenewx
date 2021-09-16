ac3b49b9a5eb61efef0d2df2f2e9c30975ac8f55
jianglei <jianglei@effigo.cn>
2021-09-16 14:54:47 +0800

    图标组件添加对非element标准图标的支持

--- tnxweb/components/vue/element-plus/icon/Icon.vue
+++ tnxweb/components/vue/element-plus/icon/Icon.vue
@@ -1,27 +1,55 @@
 <template>
-    <el-icon :class="{'is-loading': type === 'Loading'}" :size="size" :color="color">
+    <el-icon :class="className" :size="size" :color="color">
         <Loading v-if="type === 'Loading'"/>
+        <ArrowDown v-else-if="type === 'ArrowDown'"/>
         <ArrowRight v-else-if="type === 'ArrowRight'"/>
         <Edit v-else-if="type === 'Edit'"/>
+        <HomeFilled v-else-if="type === 'HomeFilled'"/>
         <OfficeBuilding v-else-if="type === 'OfficeBuilding'"/>
         <Plus v-else-if="type === 'Plus'"/>
         <Select v-else-if="type === 'Select'"/>
         <Setting v-else-if="type === 'Setting'"/>
+        <UserFilled v-else-if="type === 'UserFilled'"/>
     </el-icon>
 </template>
 
 <script>
-import {ArrowRight, Edit, Loading, OfficeBuilding, Plus, Select, Setting} from '@element-plus/icons';
+import {
+    ArrowDown,
+    ArrowRight,
+    Edit,
+    HomeFilled,
+    Loading,
+    OfficeBuilding,
+    Plus,
+    Select,
+    Setting,
+    UserFilled
+} from '@element-plus/icons';
+
+const components = {
+    ArrowDown, ArrowRight, Edit, HomeFilled, Loading, OfficeBuilding, Plus, Select, Setting, UserFilled
+};
 
 export default {
     name: 'TnxelIcon',
-    components: {
-        ArrowRight, Edit, Loading, OfficeBuilding, Plus, Select, Setting
-    },
+    components: components,
     props: {
         type: String,
         color: {},
         size: Number,
     },
+    computed: {
+        className() {
+            if (this.type && components[this.type]) {
+                if (this.type === 'Loading') {
+                    return 'is-loading';
+                }
+            } else {
+                return this.type;
+            }
+            return undefined;
+        }
+    }
 }
 </script>
