5214bd1d280ba9cbdeb7b30f7bdcd4d574a2ae94
jianglei <jianglei@effigo.cn>
2021-11-26 14:56:29 +0800

    添加长整型数组-类json字符串的属性转换器

--- tnxjee/tnxjee-repo-jpa/src/main/java/org/truenewx/tnxjee/repo/jpa/converter/LongArrayToJsonLikeAttributeConverter.java
+++ tnxjee/tnxjee-repo-jpa/src/main/java/org/truenewx/tnxjee/repo/jpa/converter/LongArrayToJsonLikeAttributeConverter.java
@@ -3,17 +3,16 @@ package org.truenewx.tnxjee.repo.jpa.converter;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
-import org.apache.commons.lang3.StringUtils;
 import org.truenewx.tnxjee.core.Strings;
-import org.truenewx.tnxjee.core.util.MathUtil;
+import org.truenewx.tnxjee.model.entity.util.EntityUtil;
 
 /**
- * 长整型数组属性转换器
+ * 长整型数组-类json字符串的属性转换器
  *
  * @author jianglei
  */
 @Converter
-public class LongArrayAttributeConverter implements AttributeConverter<long[], String> {
+public class LongArrayToJsonLikeAttributeConverter implements AttributeConverter<long[], String> {
 
     @Override
     public String convertToDatabaseColumn(long[] attribute) {
@@ -23,7 +22,7 @@ public class LongArrayAttributeConverter implements AttributeConverter<long[], S
         if (attribute.length == 0) {
             return Strings.EMPTY;
         }
-        return StringUtils.join(attribute, ','); // 不能用字符串作为分隔符，否则将被视为另一个重载方法
+        return EntityUtil.toJsonLike(attribute);
     }
 
     @Override
@@ -34,7 +33,7 @@ public class LongArrayAttributeConverter implements AttributeConverter<long[], S
         if (dbData.length() == 0) {
             return new long[0];
         }
-        return MathUtil.parseLongArray(dbData, Strings.COMMA);
+        return EntityUtil.parseJsonLike(dbData, long[].class);
     }
 
 }
