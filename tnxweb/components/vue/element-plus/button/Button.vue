508f9e12cf3f3cd703b080a1149a6011785e259b
jianglei <jianglei@effigo.cn>
2021-12-01 15:15:23 +0800

    按钮文本默认与图标保持距离

--- tnxweb/components/vue/element-plus/button/Button.vue
+++ tnxweb/components/vue/element-plus/button/Button.vue
@@ -21,10 +21,10 @@
         <el-button :type="type" :disabled="disabled" :title="title" @click="clickButton" :size="size"
             :loading="loading" :plain="plain" :autofocus="autofocus" :round="round" :circle="circle" v-else>
             <tnxel-icon :type="icon" v-if="icon"/>
-            <template v-if="!hiddenCaption">
+            <span v-if="!hiddenCaption">
                 <slot v-if="$slots.default"></slot>
                 <template v-else-if="menuItem">{{ menuItem.caption }}</template>
-            </template>
+            </span>
         </el-button>
     </el-tooltip>
 </template>
