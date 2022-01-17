7e5b12eb6ed6f2564b19539bc7b2ac8ade2051ef
jianglei <jianglei@effigo.cn>
2022-01-17 17:02:36 +0800

    添加标题支持

--- tnxweb/components/vue/element-plus/dropdown-item/DropdownItem.vue
+++ tnxweb/components/vue/element-plus/dropdown-item/DropdownItem.vue
@@ -1,7 +1,7 @@
 <template>
     <el-dropdown-item class="tnxel-dropdown-item">
         <el-tooltip :content="tooltip" :placement="tooltipPlacement" :offset="32" :disabled="!tooltip">
-            <div class="flex-v-center" v-if="icon">
+            <div class="flex-v-center" :title="title" v-if="icon">
                 <tnxel-icon :type="icon"/>
                 <span><slot></slot></span>
             </div>
@@ -15,6 +15,7 @@ export default {
     name: 'TnxelDropdownItem',
     props: {
         icon: String,
+        title: String,
         tooltip: String,
         tooltipPlacement: {
             type: String,
