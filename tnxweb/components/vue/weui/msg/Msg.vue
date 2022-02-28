3a45a0daa469abaae92e76487b4f23c58abfbae0
jianglei81 <jianglei81@sina.cn>
2022-02-28 22:11:48 +0800

    改进weui-msg组件封装

--- tnxweb/components/vue/weui/msg/Msg.vue
+++ tnxweb/components/vue/weui/msg/Msg.vue
@@ -1,16 +1,21 @@
 <template>
     <div class="weui-msg">
-        <div class="weui-msg__icon-area" v-if="icon || $slots.icon">
-            <i class="weui-icon_msg" :class="icon" v-if="icon"></i>
-            <slot name="icon"></slot>
+        <div class="weui-msg__icon-area" v-if="$slots.icon || icon">
+            <slot name="icon" v-if="$slots.icon"></slot>
+            <i class="weui-icon_msg" :class="icon" v-else></i>
         </div>
         <div class="weui-msg__text-area">
             <h2 class="weui-msg__title" v-if="title">{{ title }}</h2>
-            <p class="weui-msg__desc" v-if="desc">{{ desc }}</p>
-            <p class="weui-msg__desc-primary" v-if="secondaryDesc">{{ secondaryDesc }}</p>
+            <p class="weui-msg__desc" v-if="$slots.desc">
+                <slot name="desc"></slot>
+            </p>
+            <p class="weui-msg__desc" v-else-if="desc">{{ desc }}</p>
         </div>
-        <div class="weui-msg__opr-area" v-if="$slots.default">
+        <div class="weui-msg__custom-area" v-if="$slots.default">
             <slot></slot>
+        </div>
+        <div class="weui-msg__opr-area" v-if="$slots.opr">
+            <slot name="opr"></slot>
         </div>
         <div class="weui-msg__tips-area" v-if="tip">
             <p class="weui-msg__tips">{{ tip }}</p>
@@ -28,17 +33,11 @@ export default {
         secondaryDesc: String,
         tip: String,
     },
-    data() {
-        return {}
-    },
-    mounted() {
-    },
-    methods: {}
 }
 </script>
 
 <style>
-.weui-msg .weui-icon_msg {
+.weui-msg .weui-icon_msg:not([class*="weui-icon-"]) {
     width: unset;
     height: unset;
     font-size: 4rem;
