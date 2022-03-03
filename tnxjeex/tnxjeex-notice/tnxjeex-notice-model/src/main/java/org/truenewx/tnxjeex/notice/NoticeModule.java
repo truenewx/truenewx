aeb1970d36878b4435c920f2fc00e2029a4b19cb
jianglei81 <jianglei81@sina.cn>
2022-03-03 22:37:04 +0800

    添加通知模块定义

---
+++ tnxjeex/tnxjeex-notice/tnxjeex-notice-model/src/main/java/org/truenewx/tnxjeex/notice/NoticeModule.java
@@ -0,0 +1,7 @@
+package org.truenewx.tnxjeex.notice;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackageClasses = NoticeModule.class)
+public class NoticeModule {
+}
