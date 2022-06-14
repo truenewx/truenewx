4abbfa9651cb0caf0aaf8d8e120a4752bb6cc852
jianglei <jianglei@effigo.cn>
2022-06-14 22:36:47 +0800

    下拉菜单项内容宽度100%

--- tnxweb/src/vue/element-plus/dropdown-item/DropdownItem.vue
+++ tnxweb/src/vue/element-plus/dropdown-item/DropdownItem.vue
@@ -3,12 +3,11 @@
         <el-tooltip :content="tooltip" :placement="tooltipPlacement" :offset="32" :disabled="!tooltip">
             <div class="w-100 flex-v-center" :title="title" v-if="icon">
                 <tnxel-icon :value="icon" :size="iconSize"/>
-                <span><slot></slot></span>
+                <span class="w-100"><slot></slot></span>
             </div>
-            <div class="w-100" :title="title" v-else-if="title">
+            <div class="w-100" :title="title" v-else>
                 <slot></slot>
             </div>
-            <slot v-else></slot>
         </el-tooltip>
     </el-dropdown-item>
 </template>
