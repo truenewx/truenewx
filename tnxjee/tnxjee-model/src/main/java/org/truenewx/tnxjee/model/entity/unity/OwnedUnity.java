55a1aade8bc424ef011485069bee822d80ca4cde
jianglei <jianglei@effigo.cn>
2022-06-29 15:48:15 +0800

    抽取从属的接口

--- tnxjee/tnxjee-model/src/main/java/org/truenewx/tnxjee/model/entity/unity/OwnedUnity.java
+++ tnxjee/tnxjee-model/src/main/java/org/truenewx/tnxjee/model/entity/unity/OwnedUnity.java
@@ -2,16 +2,14 @@ package org.truenewx.tnxjee.model.entity.unity;
 
 import java.io.Serializable;
 
+import org.truenewx.tnxjee.core.spec.Owned;
+
 /**
  * 从属单体
  *
- * @author jianglei
  * @param <K> 标识类型
  * @param <O> 所属者类型
+ * @author jianglei
  */
-public interface OwnedUnity<K extends Serializable, O extends Serializable> extends Unity<K> {
-    /**
-     * @return 所有者
-     */
-    O getOwner();
+public interface OwnedUnity<K extends Serializable, O extends Serializable> extends Unity<K>, Owned<O> {
 }
