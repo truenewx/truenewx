377797e3d1419e666040cb00ab9f288c72bb1d32
jianglei <jianglei@effigo.cn>
2022-07-04 20:38:11 +0800

    可解决异常添加负载支持

--- tnxjee/tnxjee-service/src/main/java/org/truenewx/tnxjee/service/exception/ResolvableException.java
+++ tnxjee/tnxjee-service/src/main/java/org/truenewx/tnxjee/service/exception/ResolvableException.java
@@ -10,12 +10,28 @@ public abstract class ResolvableException extends RuntimeException {
 
     private static final long serialVersionUID = -4552090901512143756L;
 
+    private Object payload;
+
     public ResolvableException() {
         super();
     }
 
     public ResolvableException(String message) {
         super(message);
+    }
+
+    /**
+     * 设置负载，用于在异常处理过程中向后续处理环节传递数据
+     *
+     * @param payload 负载
+     */
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T getPayload() {
+        return (T) this.payload;
     }
 
 }
