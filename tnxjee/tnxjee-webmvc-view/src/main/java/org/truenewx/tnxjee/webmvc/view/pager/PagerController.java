3f73e828b39b85e9f60e3fceeea5ccb11db52094
jianglei <jianglei@effigo.cn>
2022-07-09 10:53:22 +0800

    分页控制器不再继承简单控制器

--- tnxjee/tnxjee-webmvc-view/src/main/java/org/truenewx/tnxjee/webmvc/view/pager/PagerController.java
+++ tnxjee/tnxjee-webmvc-view/src/main/java/org/truenewx/tnxjee/webmvc/view/pager/PagerController.java
@@ -10,20 +10,19 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.truenewx.tnxjee.webmvc.servlet.mvc.SimpleController;
 
 /**
- *
- *
  * @author jianglei
  */
 @Controller
-public class PagerController extends SimpleController {
+@RequestMapping("/pager")
+public class PagerController {
 
-    @RequestMapping(value = "/pager")
-    public ModelAndView index(HttpServletRequest request, HttpServletResponse response) {
+    @GetMapping
+    public ModelAndView get(HttpServletRequest request, HttpServletResponse response) {
         try {
             Map<String, Object> params = new HashMap<>();
             Enumeration<String> names = request.getParameterNames();
@@ -38,4 +37,5 @@ public class PagerController extends SimpleController {
         }
         return null;
     }
+
 }
