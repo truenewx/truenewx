0f652f7ee7ea667cb82e4eb460421036e9edc894
jianglei <jianglei@effigo.cn>
2022-08-12 10:59:16 +0800

    移除对tenpay的支持

--- tnxjeex/tnxjeex-payment/tnxjeex-payment-web/src/main/java/org/truenewx/tnxjeex/payment/web/AbstractPayController.java
+++ tnxjeex/tnxjeex-payment/tnxjeex-payment-web/src/main/java/org/truenewx/tnxjeex/payment/web/AbstractPayController.java
@@ -1,6 +1,5 @@
 package org.truenewx.tnxjeex.payment.web;
 
-import java.io.Reader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,7 +7,6 @@ import java.util.Map.Entry;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +18,6 @@ import org.truenewx.tnxjee.webmvc.security.config.annotation.ConfigAnonymous;
 import org.truenewx.tnxjeex.payment.core.PaymentManager;
 import org.truenewx.tnxjeex.payment.core.PaymentResult;
 import org.truenewx.tnxjeex.payment.core.gateway.PaymentGateway;
-import org.truenewx.tnxjeex.payment.core.gateway.tenpay.TenpayXmlUtil;
 
 /**
  * 抽象的支付控制器
@@ -39,10 +36,12 @@ public abstract class AbstractPayController {
     @RequestMapping(value = "/result/confirm/{gatewayName}")
     @ConfigAnonymous
     @ResponseBody
-    public String confirm(@PathVariable("gatewayName") String gatewayName, HttpServletRequest request) {
+    public String confirm(@PathVariable("gatewayName") String gatewayName,
+            HttpServletRequest request) {
         Map<String, String> params = getHttpRequestParams(request);
         if (params != null && params.size() > 0) {
-            PaymentResult result = this.paymentManager.notifyResult(gatewayName, true, null, params);
+            PaymentResult result = this.paymentManager.notifyResult(gatewayName, true, null,
+                    params);
             if (result != null) {
                 return result.getResponse();
             }
@@ -52,33 +51,23 @@ public abstract class AbstractPayController {
 
     private Map<String, String> getHttpRequestParams(HttpServletRequest request) {
         Map<String, String[]> requestParams = request.getParameterMap();
-        if (requestParams != null && requestParams.size() > 0) {
-            Map<String, String> params = new HashMap<>();
-            for (Entry<String, String[]> entry : requestParams.entrySet()) {
-                String name = entry.getKey();
-                String[] values = entry.getValue();
-                params.put(name, StringUtils.join(values, Strings.COMMA));
-            }
-            return params;
-        } else {
-            try {
-                Reader reader = request.getReader();
-                String xml = IOUtils.toString(reader);
-                reader.close();
-                return TenpayXmlUtil.doXmlParse(xml);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        Map<String, String> params = new HashMap<>();
+        for (Entry<String, String[]> entry : requestParams.entrySet()) {
+            String name = entry.getKey();
+            String[] values = entry.getValue();
+            params.put(name, StringUtils.join(values, Strings.COMMA));
         }
-        return null;
+        return params;
     }
 
     @RequestMapping(value = "/result/show/{gatewayName}/{terminal}")
     @ConfigAnonymous
     public String show(@PathVariable("gatewayName") String gatewayName,
-            @PathVariable(value = "terminal", required = false) String terminal, HttpServletRequest request) {
+            @PathVariable(value = "terminal", required = false) String terminal,
+            HttpServletRequest request) {
         Map<String, String> params = getHttpRequestParams(request);
-        PaymentResult result = this.paymentManager.notifyResult(gatewayName, false, Terminal.of(terminal), params);
+        PaymentResult result = this.paymentManager.notifyResult(gatewayName, false,
+                Terminal.of(terminal), params);
         return result == null ? null : getShowResultName(result);
     }
 
