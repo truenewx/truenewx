5c5fde172c7d5f5778c766261a12d68a692f3343
jianglei <jianglei@effigo.cn>
2022-09-08 18:01:19 +0800

    修正文件上传校验结果不正确的问题

--- tnxweb/src/vue/element-plus/fss-upload/FssUpload.vue
+++ tnxweb/src/vue/element-plus/fss-upload/FssUpload.vue
@@ -90,7 +90,7 @@ export default {
     },
     computed: {
         uploadingFiles() {
-            return this.$refs.upload?.uploadingFiles || [];
+            return this.$refs.upload?.files || [];
         },
     },
     watch: {
