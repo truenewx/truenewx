dbcd057a1f72ae5c26fc60829656427a4a943ce7
jianglei <jianglei@effigo.cn>
2022-09-30 22:14:47 +0800

    添加错误码支持

--- tnxjee/tnxjee-webmvc-view/src/main/java/org/truenewx/tnxjee/webmvc/view/tagext/ErrorTagSupport.java
+++ tnxjee/tnxjee-webmvc-view/src/main/java/org/truenewx/tnxjee/webmvc/view/tagext/ErrorTagSupport.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.servlet.ServletRequest;
 import javax.servlet.jsp.tagext.TagSupport;
 
+import org.apache.commons.lang3.StringUtils;
 import org.truenewx.tnxjee.core.Strings;
 import org.truenewx.tnxjee.service.exception.model.ExceptionError;
 import org.truenewx.tnxjee.webmvc.exception.message.ResolvableExceptionMessageSaver;
@@ -19,9 +20,14 @@ public abstract class ErrorTagSupport extends TagSupport {
     private static final long serialVersionUID = -1567572110106962210L;
 
     protected String field = Strings.ASTERISK;
+    private String code;
 
     public void setField(String field) {
         this.field = field;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
     }
 
     @SuppressWarnings("unchecked")
@@ -42,11 +48,19 @@ public abstract class ErrorTagSupport extends TagSupport {
         List<ExceptionError> errors = getErrors();
         if (errors != null) {
             if (Strings.ASTERISK.equals(this.field)) {
-                return errors.size() > 0;
+                if (StringUtils.isBlank(this.code)) {
+                    return errors.size() > 0;
+                } else {
+                    for (ExceptionError error : errors) {
+                        if (this.code.equals(error.getCode())) {
+                            return true;
+                        }
+                    }
+                }
             }
             for (ExceptionError error : errors) {
                 if (this.field.equals(error.getField())) {
-                    return true;
+                    return StringUtils.isBlank(this.code) || this.code.equals(error.getCode());
                 }
             }
         }
