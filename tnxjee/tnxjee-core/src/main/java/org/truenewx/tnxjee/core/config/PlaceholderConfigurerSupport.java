bc20079dbf8c59b27270bc458e9f705f827ed249
jianglei <jianglei@effigo.cn>
2022-10-18 19:10:44 +0800

    外部占位符配置文件加载添加jar/war运行时支持

--- tnxjee/tnxjee-core/src/main/java/org/truenewx/tnxjee/core/config/PlaceholderConfigurerSupport.java
+++ tnxjee/tnxjee-core/src/main/java/org/truenewx/tnxjee/core/config/PlaceholderConfigurerSupport.java
@@ -1,7 +1,6 @@
 package org.truenewx.tnxjee.core.config;
 
 import java.io.File;
-import java.io.IOException;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.context.annotation.Bean;
@@ -9,11 +8,15 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.truenewx.tnxjee.core.Strings;
+import org.truenewx.tnxjee.core.util.LogUtil;
 
 /**
  * 占位符配置支持。由于占位符配置器在配置属性生效前创建，所以本类下不能使用配置属性
  */
 public abstract class PlaceholderConfigurerSupport {
+
+    private static final String JAR_FILE_PREFIX = "jar:file:";
 
     @Bean
     public PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
@@ -37,19 +40,40 @@ public abstract class PlaceholderConfigurerSupport {
 
     protected Resource getLocation() {
         try {
-            Resource resource = new ClassPathResource("/");
-            File root = resource.getFile().getParentFile().getParentFile().getParentFile().getParentFile();
-            String path = root.getAbsolutePath() + "/conf/" + getAppName() + ".properties";
-            return new FileSystemResource(path);
-        } catch (IOException e) {
-            e.printStackTrace();
+            Resource resource = new ClassPathResource(Strings.SLASH);
+
+            String path = null;
+            String url = resource.getURL().toString();
+            if (url.startsWith(JAR_FILE_PREFIX)) { // 位于jar/war中
+                int index = url.indexOf("ar!/");
+                if (index > 0) {
+                    path = url.substring(JAR_FILE_PREFIX.length(), index);
+                    path = path.substring(0, path.lastIndexOf(Strings.SLASH) + 1); // 确保以/结尾
+                }
+            } else {
+                File file = resource.getFile().getParentFile().getParentFile().getParentFile();
+                if ("webapps".equals(file.getName())) { // 位于tomcat中
+                    path = file.getParentFile().getAbsolutePath() + "/conf/";
+                }
+            }
+
+            if (path != null) {
+                path += getBasename() + ".properties";
+                resource = new FileSystemResource(path);
+                if (resource.exists()) {
+                    System.out.println("====== Placeholder property source found: " + resource.getURL());
+                    return resource;
+                }
+            }
+        } catch (Exception e) {
+            LogUtil.error(getClass(), e);
         }
         return null;
     }
 
     /**
-     * @return 当前应用的名称
+     * @return 不含扩展名的配置文件基础名称，需确保在配置目录中唯一
      */
-    protected abstract String getAppName();
+    protected abstract String getBasename();
 
 }
