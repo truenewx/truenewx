b311db153b33e874117044f17536473d9506576e
jianglei <jianglei@effigo.cn>
2022-10-21 11:46:14 +0800

    处理登录成功后处理过程中可能出现的业务异常

--- tnxjeex/tnxjeex-cas/tnxjeex-cas-server/src/main/java/org/truenewx/tnxjeex/cas/server/authentication/CasAuthenticationSuccessHandler.java
+++ tnxjeex/tnxjeex-cas/tnxjeex-cas-server/src/main/java/org/truenewx/tnxjeex/cas/server/authentication/CasAuthenticationSuccessHandler.java
@@ -11,8 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.web.servlet.ModelAndView;
 import org.truenewx.tnxjee.core.util.NetUtil;
+import org.truenewx.tnxjee.service.exception.ResolvableException;
 import org.truenewx.tnxjee.web.util.WebUtil;
+import org.truenewx.tnxjee.webmvc.servlet.mvc.WebMvcViewResolver;
+import org.truenewx.tnxjee.webmvc.view.exception.resolver.ViewResolvableExceptionResolver;
 import org.truenewx.tnxjee.webmvc.view.util.WebViewUtil;
 import org.truenewx.tnxjeex.cas.core.CasConstants;
 import org.truenewx.tnxjeex.cas.server.service.CasServiceManager;
@@ -27,20 +31,34 @@ public class CasAuthenticationSuccessHandler implements AuthenticationSuccessHan
     private CasServiceManager serviceManager;
     @Autowired
     private CasTicketManager ticketManager;
+    @Autowired
+    private ViewResolvableExceptionResolver resolvableExceptionResolver;
+    @Autowired
+    private WebMvcViewResolver mvcViewResolver;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication authentication) throws IOException, ServletException {
-        this.ticketManager.createTicketGrantingTicket(request, response);
-        CasUserSpecificDetailsAuthenticationToken token = (CasUserSpecificDetailsAuthenticationToken) authentication;
-        String service = token.getService();
-        String scope = token.getScope();
-        String targetUrl = this.serviceManager.getLoginProcessUrl(request, service, scope);
-        Map<String, Object> parameters = WebUtil.getRequestParameterMap(request, "username",
-                "password", CasConstants.PARAMETER_SERVICE, CasConstants.PARAMETER_SCOPE);
-        targetUrl = NetUtil.mergeParams(targetUrl, parameters, StandardCharsets.UTF_8.name());
-        // 此处一定是表单提交鉴权成功，无需AjaxRedirectStrategy
-        WebViewUtil.redirect(request, response, targetUrl);
+        try {
+            this.ticketManager.createTicketGrantingTicket(request, response);
+            CasUserSpecificDetailsAuthenticationToken token = (CasUserSpecificDetailsAuthenticationToken) authentication;
+            String service = token.getService();
+            String scope = token.getScope();
+            String targetUrl = this.serviceManager.getLoginProcessUrl(request, service, scope);
+            Map<String, Object> parameters = WebUtil.getRequestParameterMap(request, "username",
+                    "password", CasConstants.PARAMETER_SERVICE, CasConstants.PARAMETER_SCOPE);
+            targetUrl = NetUtil.mergeParams(targetUrl, parameters, StandardCharsets.UTF_8.name());
+            // 此处一定是表单提交鉴权成功，无需AjaxRedirectStrategy
+            WebViewUtil.redirect(request, response, targetUrl);
+        } catch (ResolvableException e) {
+            ModelAndView mav = this.resolvableExceptionResolver.resolveException(request, response, null, e);
+            if (mav != null) {
+                String viewName = mav.getViewName();
+                if (viewName != null) {
+                    this.mvcViewResolver.resolveView(request, response, viewName, mav.getModel());
+                }
+            }
+        }
     }
 
 }
