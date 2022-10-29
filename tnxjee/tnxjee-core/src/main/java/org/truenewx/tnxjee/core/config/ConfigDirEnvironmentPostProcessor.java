0fad159d0ae3ccc0f676b1a8d8a222cd53b0e9b4
jianglei <jianglei@effigo.cn>
2022-10-29 10:45:45 +0800

    配置文件基本名称对外公开

--- tnxjee/tnxjee-core/src/main/java/org/truenewx/tnxjee/core/config/ConfigDirEnvironmentPostProcessor.java
+++ tnxjee/tnxjee-core/src/main/java/org/truenewx/tnxjee/core/config/ConfigDirEnvironmentPostProcessor.java
@@ -32,7 +32,7 @@ public class ConfigDirEnvironmentPostProcessor implements EnvironmentPostProcess
 
     private static final String PROPERTY_SOURCE_NAME_PREFIX = Framework.NAME + " property source: ";
     private static final String DIR_NAME = "config";
-    private static final String BASENAME = "application";
+    public static final String BASENAME = "application";
     private static final String PROPERTY_ENV_CONFIG_PRINT = "env.config.print";
 
     private ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
