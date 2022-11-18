75b807d070dfd246acf9224e7e7494f3e6791cc3
jianglei <jianglei@effigo.cn>
2022-11-18 21:34:07 +0800

    改进错误显示页体验

--- tnxweb/src/vue/element-plus/close-error-button/CloseErrorButton.vue
+++ tnxweb/src/vue/element-plus/close-error-button/CloseErrorButton.vue
@@ -5,13 +5,19 @@
 <script>
 export default {
     name: 'TnxelCloseErrorButton',
+    props: {
+        prev: String,
+    },
     data() {
-        return {
-            backable: window.history.length > 0,
-            closeable: window.opener !== undefined,
-        };
+        return {};
     },
     computed: {
+        backable() {
+            return this.prev || window.history.length > 2; // 前两个为新标签页和当前页，需排除
+        },
+        closeable() {
+            return window.opener !== undefined && window.opener !== null;
+        },
         text() {
             if (this.backable) {
                 return '返回';
@@ -25,7 +31,11 @@ export default {
     methods: {
         close() {
             if (this.backable) {
-                window.history.back();
+                if (this.prev) {
+                    window.location.href = this.prev;
+                } else {
+                    window.history.back();
+                }
             } else if (this.closeable) {
                 window.close();
             }
