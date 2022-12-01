e17e46cafcca59ed579e2f2b312afd45d2ee0dae
jianglei <jianglei@effigo.cn>
2022-12-01 21:07:06 +0800

    JwtHelper更名为RpcHelper，添加RPC调用方法

--- tnxjee/tnxjee-webmvc/src/main/java/org/truenewx/tnxjee/webmvc/util/RpcHelper.java
+++ tnxjee/tnxjee-webmvc/src/main/java/org/truenewx/tnxjee/webmvc/util/RpcHelper.java
@@ -1,21 +1,28 @@
-package org.truenewx.tnxjee.webmvc.jwt;
+package org.truenewx.tnxjee.webmvc.util;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.truenewx.tnxjee.core.Strings;
+import org.truenewx.tnxjee.core.spec.HttpRequestMethod;
+import org.truenewx.tnxjee.core.util.ExceptionUtil;
+import org.truenewx.tnxjee.core.util.HttpClientUtil;
+import org.truenewx.tnxjee.core.util.tuple.Binate;
 import org.truenewx.tnxjee.model.spec.user.security.UserSpecificDetails;
 import org.truenewx.tnxjee.web.util.WebConstants;
+import org.truenewx.tnxjee.webmvc.jwt.JwtGenerator;
 
 /**
- * JWT协助者
+ * RPC协助者
  *
  * @author jianglei
  */
 @Component
-public class JwtHelper {
+public class RpcHelper {
 
     @Autowired
     private JwtGenerator generator;
@@ -37,6 +44,21 @@ public class JwtHelper {
             String jwt = this.generator.generate(type, userDetails);
             headers.put(WebConstants.HEADER_RPC_JWT, jwt);
         }
+    }
+
+    public String call(HttpRequestMethod method, String url, Map<String, Object> params, String type,
+            UserSpecificDetails<?> userSpecificDetails) {
+        try {
+            Map<String, String> headers = generateHeaders(type, userSpecificDetails);
+            Binate<Integer, String> result = HttpClientUtil.request(method, url, params, headers,
+                    Strings.ENCODING_UTF8);
+            if (result != null && result.getLeft() == HttpStatus.SC_OK) {
+                return result.getRight();
+            }
+        } catch (Exception e) {
+            throw ExceptionUtil.toRuntimeException(e);
+        }
+        return null;
     }
 
 }
