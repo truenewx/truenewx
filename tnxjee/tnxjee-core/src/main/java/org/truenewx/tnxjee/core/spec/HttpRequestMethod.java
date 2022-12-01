7f42fff388f2f1a47d9ec1bd1ae628e8a0c593e1
jianglei <jianglei@effigo.cn>
2022-12-01 21:06:16 +0800

    常量声明与spring的HttpMethod保持一致

--- tnxjee/tnxjee-core/src/main/java/org/truenewx/tnxjee/core/spec/HttpRequestMethod.java
+++ tnxjee/tnxjee-core/src/main/java/org/truenewx/tnxjee/core/spec/HttpRequestMethod.java
@@ -4,22 +4,9 @@ package org.truenewx.tnxjee.core.spec;
  * HTTP请求方式
  *
  * @author jianglei
- * 
  */
 public enum HttpRequestMethod {
 
-    GET,
-
-    POST,
-
-    PUT,
-
-    DELETE,
-
-    HEAD,
-
-    OPTIONS,
-
-    TRACE;
+    GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE;
 
 }
