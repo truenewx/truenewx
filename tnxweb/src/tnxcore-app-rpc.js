1c866c62349a0e5f0d87124c5f8236ee2d4450db
jianglei <jianglei@effigo.cn>
2023-02-16 20:36:15 +0800

    Revert "修正私有化配置程序登录失效重登录后跳转地址错误的问题"
    This reverts commit d93c4b42e0589312f700633b3b57bc0e1b251821.

--- tnxweb/src/tnxcore-app-rpc.js
+++ tnxweb/src/tnxcore-app-rpc.js
@@ -219,18 +219,17 @@ export default {
                                 }
                                 loginUrl += _this.loginSuccessRedirectParameter + '=' + loginSuccessRedirectUrl;
                             }
-                            // 原始地址是授权验证地址或登出地址，视为框架特有请求，无需应用做个性化处理
-                            if (originalUrl && (originalUrl.startsWith(_this.authenticationContextUrl + '/')
-                                || originalUrl === _this.logoutProcessUrl)) {
-                                originalUrl = undefined;
-                                originalMethod = undefined;
-                            }
-                            const toLogin = options.toLogin || _this.toLogin;
-                            if (toLogin(loginUrl, originalUrl, originalMethod) !== false) {
-                                return;
-                            }
                         }
-                        _this.handleOtherError(url + ':\n' + error, options);
+                        // 原始地址是授权验证地址或登出地址，视为框架特有请求，无需应用做个性化处理
+                        if (originalUrl && (originalUrl.startsWith(_this.authenticationContextUrl + '/')
+                            || originalUrl === _this.logoutProcessUrl)) {
+                            originalUrl = undefined;
+                            originalMethod = undefined;
+                        }
+                        const toLogin = options.toLogin || _this.toLogin;
+                        if (toLogin(loginUrl, originalUrl, originalMethod)) {
+                            return;
+                        }
                         break;
                     }
                     case 400: {
